0d55893e9eabf8a0876ad17872582676b1495f79
flatsiedatsie <unfold@gmail.com>
2019-04-30 22:41:08 +0000

    Correct utility file

--- pkg/util.py
+++ pkg/util.py
@@ -1,33 +1,7 @@
 """Utility functions."""
 
-def pretty(d, indent=0):
-    for key, value in d.items():
-        print('\t' * indent + str(key))
-        if isinstance(value, dict):
-            pretty(value, indent+1)
-        else:
-            print('\t' * (indent+1) + str(value))
-            
-def is_a_number(s):
-    """ Returns True is string is a number. """
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
-    
-def make_safe_name(s):
-    keepcharacters = (' ','.','_')
-    return "".join(c for c in s if c.isalpha() or c.isalnum() or c in keepcharacters).rstrip()
+def validip(ip):
+    return ip.count('.') == 3 and  all(0<=int(num)<256 for num in ip.rstrip().split('.'))
 
-    
-def get_int_or_float(v):
-    number_as_float = float(v)
-    number_as_int = int(number_as_float)
-    if number_as_float == number_as_int:
-        return number_as_int
-    else:
-        return number_as_float
-        #return  float('%.2f' % number_as_float).rstrip('0').rstrip('.')
-        #return  round(number_as_float,2)
-
+def clamp(n, minn, maxn):
+    return max(min(maxn, n), minn)
