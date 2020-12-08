5d578e3a005b63e29f4e8e74aa295628a83c6c71
flatsiedatsie <unfold@gmail.com>
2020-12-08 13:50:45 +0100

    1.1.3
    automation

--- package.sh
+++ package.sh
@@ -2,17 +2,28 @@
 
 version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
 
+# Setup environment for building inside Dockerized toolchain
+[ $(id -u) = 0 ] && umask 0
+
 # Clean up from previous releases
-rm -rf *.tgz package SHA256SUMS
+rm -rf *.tgz *.sha256sum package SHA256SUMS lib
+
+if [ -z "${ADDON_ARCH}" ]; then
+  TARFILE_SUFFIX=
+else
+  PYTHON_VERSION="$(python3 --version 2>&1 | cut -d' ' -f2 | cut -d. -f 1-2)"
+  TARFILE_SUFFIX="-${ADDON_ARCH}-v${PYTHON_VERSION}"
+fi
 
 # Prep new package
-mkdir package
+mkdir -p package
 
 # Put package together
 cp -r pkg LICENSE *.json *.py package/
 find package -type f -name '*.pyc' -delete
 find package -type f -name '._*' -delete
 find package -type d -empty -delete
+rm -rf package/pkg/pycache
 
 # Generate checksums
 echo "generating checksums"
@@ -28,4 +39,4 @@ tar czf ${TARFILE} package
 shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
 cat ${TARFILE}.sha256sum
 
-rm -rf SHA256SUMS package
+#rm -rf SHA256SUMS package
