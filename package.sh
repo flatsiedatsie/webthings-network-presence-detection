fa7f0e6a3c2536bf67fcba9f2f4c0a96ab387d6a
flatsiedatsie <unfold@gmail.com>
2023-12-18 14:34:57 +0100

    Update package.sh

--- package.sh
+++ package.sh
@@ -6,7 +6,7 @@ version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
 [ $(id -u) = 0 ] && umask 0
 
 # Clean up from previous releases
-rm -rf *.tgz *.sha256sum package SHA256SUMS lib
+rm -rf *.tgz *.sha256sum package SHA256SUMS
 
 if [ -z "${ADDON_ARCH}" ]; then
   TARFILE_SUFFIX=
@@ -16,16 +16,19 @@ else
 fi
 
 # Prep new package
-mkdir -p package
+mkdir -p lib package
+
+# Pull down Python dependencies
+pip3 install -r requirements.txt -t lib --no-binary :all: --prefix ""
+
 
 # Put package together
-cp -r pkg LICENSE *.json *.py package/
+cp -r pkg LICENSE manifest.json *.py js images views css README.md lib package/
 find package -type f -name '*.pyc' -delete
 find package -type f -name '._*' -delete
 find package -type d -empty -delete
 rm -rf package/pkg/pycache
 
-# Generate checksums
 echo "generating checksums"
 cd package
 find . -type f \! -name SHA256SUMS -exec shasum --algorithm 256 {} \; >> SHA256SUMS
@@ -33,10 +36,9 @@ cd -
 
 # Make the tarball
 echo "creating archive"
-TARFILE="network-presence-detection-adapter-${version}.tgz"
+TARFILE="network-presence-detection-adapter-${version}${TARFILE_SUFFIX}.tgz"
 tar czf ${TARFILE} package
 
+echo "creating shasums"
 shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
 cat ${TARFILE}.sha256sum
-
-#rm -rf SHA256SUMS package
